726883c8e0732f25232eda55efd52e4bbc4456eb
Lexe <ngocle.ng1001@gmail.com>
2024-04-11 16:12:35 +0700

    refactor: apikey middleware

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
-import { Headers, StatusCode } from "@/constants";
+import { Headers } from "@/constants";
 import apiKeyService from "@/services/apiKey.service";
+import { ForbiddenError } from "@/core/error.response";
 
 export const apiKey = async (
   req: Request,
@@ -8,20 +9,20 @@ export const apiKey = async (
   next: NextFunction
 ) => {
   const apiKey = req.headers[Headers.API_KEY];
-  if (!apiKey || Array.isArray(apiKey)) {
-    res.status(StatusCode.FORBIDDEN).json({
-      message: "You do not have permission to access this resource",
-    });
-    return;
-  }
+
+  const forbiddenError = new ForbiddenError(
+    "You do not have permission to access this resource"
+  );
+  if (!apiKey || Array.isArray(apiKey)) return next(forbiddenError);
 
   const apiKeyDocument = await apiKeyService.findApiKeyByKey(apiKey);
-  if (!apiKeyDocument) {
-    res.status(StatusCode.FORBIDDEN).json({
-      message: "You do not have permission to access this resource",
-    });
-    return;
-  }
+  if (!apiKeyDocument) return next(forbiddenError);
 
   next();
 };
+
+export const authentication = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {};
